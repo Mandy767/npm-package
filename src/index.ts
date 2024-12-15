1ea5be5eea87c158268e529fb18b5a8eeec0ce63
Mandy767 <mandardighe6@gmail.com>
2024-12-15 23:20:03 +0530

    fixes

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from "./components/Loader/Loader";
 export * from "./components/Loader/types";
 export * from "./components/Loader/useLoader";
 export * from "./components/Loader/reducer";
+export * from "./components/Loader/styles.css";
